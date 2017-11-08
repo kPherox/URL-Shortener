61b3e3716851950f27941968cb21dd981dc7de5f
kPherox <admin@mail.kr-kp.com>
2017-11-09 02:29:05 +0900

    Centering pagination

--- resources/views/vendor/pagination/default.blade.php
+++ resources/views/vendor/pagination/default.blade.php
@@ -1,40 +1,42 @@
 @if ($paginator->count())
-    <ul class="pagination">
-        {{-- Previous Page Link --}}
-        @if ($paginator->onFirstPage())
-            <li class="disabled"><span>&laquo;&laquo;</span></li>
-            <li class="disabled"><span>&laquo;</span></li>
-        @else
-            <li><a href="{{ $paginator->url(1) }}" rel="first">&laquo;&laquo;</a></li>
-            <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">&laquo;</a></li>
-        @endif
-
-        {{-- Pagination Elements --}}
-        @foreach ($elements as $element)
-            {{-- "Three Dots" Separator --}}
-            @if (is_string($element))
-                <li class="disabled"><span>{{ $element }}</span></li>
+    <div class="text-center">
+        <ul class="pagination">
+            {{-- Previous Page Link --}}
+            @if ($paginator->onFirstPage())
+                <li class="disabled"><span>&laquo;&laquo;</span></li>
+                <li class="disabled"><span>&laquo;</span></li>
+            @else
+                <li><a href="{{ $paginator->url(1) }}" rel="first">&laquo;&laquo;</a></li>
+                <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">&laquo;</a></li>
             @endif
 
-            {{-- Array Of Links --}}
-            @if (is_array($element))
-                @foreach ($element as $page => $url)
-                    @if ($page== $paginator->currentPage())
-                        <li class="active"><span>{{ $page }}</span></li>
-                    @else
-                        <li><a href="{{ $url }}">{{ $page }}</a></li>
-                    @endif
-                @endforeach
-            @endif
-        @endforeach
+            {{-- Pagination Elements --}}
+            @foreach ($elements as $element)
+                {{-- "Three Dots" Separator --}}
+                @if (is_string($element))
+                    <li class="disabled"><span>{{ $element }}</span></li>
+                @endif
 
-        {{-- Next Page Link --}}
-        @if ($paginator->hasMorePages())
-            <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">&raquo;</a></li>
-            <li><a href="{{ $paginator->url($paginator->lastPage()) }}" rel="last">&raquo;&raquo;</a></li>
-        @else
-            <li class="disabled"><span>&raquo;</span></li>
-            <li class="disabled"><span>&raquo;&raquo;</span></li>
-        @endif
-    </ul>
+                {{-- Array Of Links --}}
+                @if (is_array($element))
+                    @foreach ($element as $page => $url)
+                        @if ($page== $paginator->currentPage())
+                            <li class="active"><span>{{ $page }}</span></li>
+                        @else
+                            <li><a href="{{ $url }}">{{ $page }}</a></li>
+                        @endif
+                    @endforeach
+                @endif
+            @endforeach
+
+            {{-- Next Page Link --}}
+            @if ($paginator->hasMorePages())
+                <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">&raquo;</a></li>
+                <li><a href="{{ $paginator->url($paginator->lastPage()) }}" rel="last">&raquo;&raquo;</a></li>
+            @else
+                <li class="disabled"><span>&raquo;</span></li>
+                <li class="disabled"><span>&raquo;&raquo;</span></li>
+            @endif
+        </ul>
+    </div>
 @endif
