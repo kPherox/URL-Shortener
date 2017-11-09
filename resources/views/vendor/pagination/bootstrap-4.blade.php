845019bc7966d2af276b9f4d2c27933ffd0d9619
kPherox <admin@mail.kr-kp.com>
2017-11-09 23:06:39 +0900

    Paginarion using view for bootstrap v4

--- resources/views/vendor/pagination/bootstrap-4.blade.php
+++ resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -1,9 +1,11 @@
 @if ($paginator->hasPages())
-    <ul class="pagination">
+    <ul class="pagination justify-content-center mt-3">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
+            <li class="page-item disabled"><span class="page-link">&laquo;&laquo;</span></li>
             <li class="page-item disabled"><span class="page-link">&laquo;</span></li>
         @else
+            <li class="page-item"><a class="page-link" href="{{ $paginator->url(1) }}" rel="first">&laquo;&laquo;</a></li>
             <li class="page-item"><a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">&laquo;</a></li>
         @endif
 
@@ -29,8 +31,10 @@
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
             <li class="page-item"><a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">&raquo;</a></li>
+            <li class="page-item"><a class="page-link" href="{{ $paginator->url($paginator->lastPage()) }}" rel="last">&raquo;&raquo;</a></li>
         @else
             <li class="page-item disabled"><span class="page-link">&raquo;</span></li>
+            <li class="page-item disabled"><span class="page-link">&raquo;&raquo;</span></li>
         @endif
     </ul>
 @endif
