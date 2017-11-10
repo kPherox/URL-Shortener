9641b431a477c5644bdd493d5d309d5d48a19e0f
kPherox <admin@mail.kr-kp.com>
2017-11-11 00:07:04 +0900

    Forgot replace panel-heading to card-header...

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-lg-8 offset-lg-2">
             <div class="card">
-                <div class="card-heading">Login</div>
+                <div class="card-header">Login</div>
 
                 <div class="card-body">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-lg-8 offset-lg-2">
             <div class="card">
-                <div class="card-heading">Register</div>
+                <div class="card-header">Register</div>
 
                 <div class="card-body">
                     <form class="form-horizontal" method="POST" action="{{ route('register') }}">
