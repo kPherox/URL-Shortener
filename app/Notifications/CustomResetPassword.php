50411a4438ab9b98d0cafdb7a814cf0204aa4ece
kPherox <admin@mail.kr-kp.com>
2017-11-13 17:30:54 +0900

    Support subdirectory for link of password reset mail

---
+++ app/Notifications/CustomResetPassword.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class CustomResetPassword extends ResetPassword
+{
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+        parent::__construct($token);
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('You are receiving this email because we received a password reset request for your account.')
+                    ->action('Reset Password', route('password.reset', $this->token))
+                    ->line('If you did not request a password reset, no further action is required.');
+    }
+}
